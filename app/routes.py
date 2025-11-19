c1d1275bdd44a081e13aaa69d2c8e98973004e17
Sivaldi vieira <sivaldovieiradealmeida@gmail.com>
2025-11-18 21:46:27 -0300

    Add templates/index/about

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, render_template
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return jsonify({'message': 'Hello, Flask CI/CD!'})
+    # Agora renderiza a página HTML em vez de JSON
+    return render_template('index.html')
+
+@main.route('/about')
+def about():
+    # Exemplo de outra rota
+    return render_template('about.html')
