71ac5ff45e897319729aef89c522261d8ecedfd8
Sivaldi vieira <sivaldovieiradealmeida@gmail.com>
2025-11-18 22:05:19 -0300

    Add templates/index/about

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 
+# A função create_app deve estar aqui
 def create_app():
+    # 1. Cria a instância do Flask
     app = Flask(__name__)
 
-    from .routes import main
-    app.register_blueprint(main)
+    # 2. Configuração (se houver)
+    # app.config.from_object('config.Config')
+
+    # 3. Importa e Registra as Blueprints (rotas)
+    from .routes import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     return app
